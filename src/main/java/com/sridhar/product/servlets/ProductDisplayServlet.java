266dab41bad92fe5512494bcca80ce576ef0d31d
sridhar <paddu_sri9@yahoo.co.in>
2017-08-23 23:20:38 +0530

    23Aug2017

---
+++ src/main/java/com/sridhar/product/servlets/ProductDisplayServlet.java
@@ -0,0 +1,31 @@
+package com.sridhar.product.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProductDisplayServlet
+ */
+public class ProductDisplayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductDisplayServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
