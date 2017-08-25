84600f3283ab2f9a3c2f2a2154c2ef0cff0d618d
sridhar <paddu_sri9@yahoo.co.in>
2017-08-25 12:13:06 +0530

    25Aug

--- src/main/java/com/sridhar/product/servlets/CreateServlet.java
+++ src/main/java/com/sridhar/product/servlets/CreateServlet.java
@@ -1,10 +1,18 @@
 package com.sridhar.product.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.sridhar.product.Product;
+import com.sridhar.product.bo.ProductServiceBO;
 
 /**
  * Servlet implementation class CreateServlet
@@ -12,19 +20,26 @@ import javax.servlet.http.HttpServletResponse;
 public class CreateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CreateServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		Integer id=Integer.parseInt(request.getParameter("id"));
+		String name= request.getParameter("name");
+		Integer price=Integer.parseInt(request.getParameter("price"));
+		
+		Product p=new Product();
+		
+		p.setId(id);
+		p.setName(name);
+		p.setPrice(price);
+		
+		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
+		ProductServiceBO bean = (ProductServiceBO) context.getBean("productBO");
+		bean.createProduct(p);
+		
+		PrintWriter out = response.getWriter();
+		out.println("new product created!!");
+		
 	}
 
 }
--- src/main/java/com/sridhar/product/servlets/ProductDisplayServlet.java
+++ src/main/java/com/sridhar/product/servlets/ProductDisplayServlet.java
@@ -1,10 +1,18 @@
 package com.sridhar.product.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.sridhar.product.Product;
+import com.sridhar.product.bo.ProductServiceBO;
 
 /**
  * Servlet implementation class ProductDisplayServlet
@@ -25,7 +33,19 @@ public class ProductDisplayServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
+		ProductServiceBO bean = (ProductServiceBO) context.getBean("productBO");
+		Product p=bean.fetchProduct(Integer.parseInt(request.getParameter("id")));
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("--- Product Details ---");
+		out.println("Product Id: "+p.getId());
+		out.println("Product Name: "+p.getName());
+		out.println("Product Price: "+p.getPrice());
+		
 	}
 
 }
